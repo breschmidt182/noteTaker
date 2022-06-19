9cfa5561d3d58e6352b6554f79d770e858f2cace
Bre <breanna.schmidt12@gmail.com>
2022-06-19 17:03:06 -0400

    added handleNoteView function

--- challenge11/routes/api/notes.js
+++ challenge11/routes/api/notes.js
@@ -1,8 +1,14 @@
 const notes = require('express').Router();
 const {readAndWriteFile, readFromFile} = require('../../db/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json')
+    .then((data) => {
+        console.log(data);
+        res.json(JSON.parse(data))
+    })
+    handleNoteView( './db/db.json')
     .then((data) => {
         console.log(data);
         res.json(JSON.parse(data))
@@ -13,4 +19,9 @@ notes.post('/', (req,res) => {
     readAndWriteFile(req.body, './db/db.json')
     res.send(req.body)
 })
+
+notes.delete('/', (req, res) => {
+
+})
+
 module.exports = notes
