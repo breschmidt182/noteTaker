376ffcd755127e534b6e79a24c58eecb991804b4
Bre <breanna.schmidt12@gmail.com>
2022-06-19 22:25:37 -0400

    add heroku

--- challenge11/routes/api/notes.js
+++ challenge11/routes/api/notes.js
@@ -1,7 +1,6 @@
 const notes = require('express').Router();
 const {readAndWriteFile, readFromFile, getNoteIndex} = require('../../db/fsUtils');
-const randomId = require('uuid');
-
+const crypto = require('crypto');
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json')
     .then((data) => {
@@ -20,12 +19,12 @@ notes.get('/', (req, res) => {
 
 notes.post('/', (req,res) => {
     readAndWriteFile(req.body, './db/db.json')
-    randomId;
+    console.log(crypto.randomUUID());
     res.send(req.body)
 })
 
 notes.delete('/', (req, res) => {
-
+    deleteNote(req.body, '1')
 })
 
 module.exports = notes
